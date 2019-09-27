8e347670731e91704fc5bb87b1179371bc8e9da3
davidbrandontiket <david.brandon@tiket.com>
2019-09-27 15:40:58 +0700

    create setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bigquery",
+    version="0.0.1",
+    author="David Brandon",
+    author_email="david.wilando4@gmail.com",
+    description="build in function to connect to bigquery",
+    url="https://github.com/davidbrandon16/python-lib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
