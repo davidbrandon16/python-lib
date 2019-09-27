5b1a686c757468d82f535b865ee2da0794e766a9
davidbrandontiket <david.brandon@tiket.com>
2019-09-27 18:35:44 +0700

    change github

--- bigquery_custom/setup.py
+++ bigquery_custom/setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="bigquery-custom-davidbrandon16",
     version="0.0.1",
     author="David Brandon Wilando",
@@ -12,8 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/davidbrandon/python-lib",
-    packages=setuptools.find_packages(),
-    scripts=['__init__.py'] ,
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
