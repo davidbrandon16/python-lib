e0f0cfe01c06ae14afd8462288ade6e4277ded34
davidbrandontiket <david.brandon@tiket.com>
2019-09-27 17:21:13 +0700

    change setup.py

--- bigquery_custom/setup.py
+++ bigquery_custom/setup.py
@@ -19,4 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
+    install_requires=[
+              'google-cloud-bigquery',
+          ]
 )
